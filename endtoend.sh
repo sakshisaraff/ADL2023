1b7f94797396a9ec35699283bac1bad623ff9112
RicBan20 <rbanthia.rb@gmail.com>
2023-12-07 16:02:10 +0000

    updated endtoend.sh

--- endtoend.sh
+++ endtoend.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 
 #SBATCH --job-name=coursework
-#SBATCH --partition=teach_gpu
+#SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --account=COMS030144
-#SBATCH -o ./log.out # STDOUT out
-#SBATCH -e ./log.err # STDERR out
+#SBATCH -o ./log_%j.out # STDOUT out
+#SBATCH -e ./log_%j.err # STDERR out
 #SBATCH --gres=gpu:1
-#SBATCH --time=1:00:00
+#SBATCH --time=6:00:00
 #SBATCH --mem=16GB
 
-# get rid of any modules already loaded
 module purge
-# load in the module dependencies for this script
 module add python
 module load python
 module load "languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch"
 
-python endtoend.py --epoch=10 --batch-size=10 --model="Deep" --eval-frequency=1
+##uncomment to run the basic CNN model with length and stride of 256 for strided convolution
+python endtoend.py --model="Basic"
+
+##uncomment to run the basic CNN model with different lengths and strides for the strided convolution
+##change the values input into --length-conv and --stride-conv according to the lengths and strides, respectively
+#python endtoend.py --model="Basic" --length-conv=1024 --stride-conv=512
+
+##uncomment to run the basic extension to the CNN model- group norm and dropout
+#python endtoend.py --epoch=40 --model="Extension1"
+
+##uncomment to run the deep CNN model- extension part 2
+#python endtoend.py --model="Deep"
